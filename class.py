d4e1800434d215b05d0007908661e975ade8c1d8
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2022-08-17 00:28:30 +0600

    new his class is addedd

--- class.py
+++ class.py
@@ -10,3 +10,16 @@ class myclas():
 obj= myclas()
 
 print(obj.age,obj.foo)
+
+class hisclas():
+    def __init__(self):
+        self.myname="His name is Shaon."
+        self.hisage="And his age is 22"
+
+obj = hisclas()
+print(obj.myname,obj.hisage)
+"""how can a object can name both class 
+at the same time. find the answer""" 
+
+obj2 = hisclas()
+print(obj2.myname,obj2.hisage)
