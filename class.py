1e69a435bae97f1ede5fd2598bb09b300800ff11
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2022-08-17 00:21:43 +0600

    myclass is added

--- class.py
+++ class.py
@@ -1,3 +1,12 @@
 from module import *
 
 EmployeID()
+
+class myclas():
+    def __init__(self):
+        self.age ="employe age is 49."
+        self.foo="And his name is Khan"
+        
+obj= myclas()
+
+print(obj.age,obj.foo)
